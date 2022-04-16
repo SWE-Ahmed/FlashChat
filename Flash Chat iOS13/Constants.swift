c197023dd78e639ee0282a2821668c27d2dd4968
SWE-Ahmed <master.necro15@gmail.com>
2022-04-16 03:53:31 +0300

    project upload
    clear

--- Flash Chat iOS13/Constants.swift
+++ Flash Chat iOS13/Constants.swift
@@ -1,11 +1,3 @@
-//
-//  Constants.swift
-//  Flash Chat iOS13
-//
-//  Created by Ahmed Almohammed on 25/02/2022.
-//  Copyright © 2022 Angela Yu. All rights reserved.
-//
-
 
 struct K {
     static let title = "⚡️FlashChat"
